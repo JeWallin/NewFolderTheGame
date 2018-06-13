5ef7cb102ecf54aad52cce8f4c6668bb98ff53f0
AlexanderSilasson <Alexander.silasson@xlent.se>
2018-06-13 12:34:25 +0200

    added playerB

--- script/ConstantManager.js
+++ script/ConstantManager.js
@@ -5,5 +5,5 @@ const imageList =
     SLIME: document.getElementById("slimeImage"),
 };
 
-const KEYS = {UP:38, DOWN:40, RIGHT:39, LEFT:37, W:87, A:65, S:83, D:68, SPACE:32, Q:81, E:69, R:82, K:75, L:76, I:186, O:79, I:73, P:80};
+const KEYS = {UP:38, DOWN:40, RIGHT:39, LEFT:37, W:87, A:65, S:83, D:68, SPACE:32, Q:81, E:69, R:82, K:75, L:76, J:74, O:79, I:73, P:80, U:85};
 
--- script/NewFolderTheGame.js
+++ script/NewFolderTheGame.js
@@ -20,6 +20,9 @@ class NewFolderTheGame
 
         this.player = new BasePlayer();
         this.player.Init(this.keyManager, KEYS.W, KEYS.S, KEYS.A, KEYS.D, KEYS.SPACE, KEYS.E, KEYS.R);
+
+        this.playerB = new BasePlayer();
+        this.playerB.Init(this.keyManager, KEYS.I, KEYS.K, KEYS.J, KEYS.L, KEYS.LEFT, KEYS.U, KEYS.O);    
     }
 
     Init()
@@ -43,6 +46,7 @@ class NewFolderTheGame
     Update(deltaTime)
     {
         this.player.Update(deltaTime);
+        this.playerB.Update(deltaTime);
     }
 
     Render()
@@ -50,7 +54,9 @@ class NewFolderTheGame
         this.gameRenderer.ClearScreen();
         var renderobj = this.obj.RenderObject();
         var renderPlayer = this.player.RenderObject();
+        var renderPlayerB = this.playerB.RenderObject();
         this.gameRenderer.Render(renderobj);
         this.gameRenderer.Render(renderPlayer);
+        this.gameRenderer.Render(renderPlayerB);
     }
 }
